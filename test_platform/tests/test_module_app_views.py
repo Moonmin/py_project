a572bf26660349aa673a3fe2d2482611d4387af9
huangxa09@gmail.com <huangxa09@gmail.com>
2018-10-31 17:14:56 +0800

    module_views单测

--- test_platform/tests/test_module_app_views.py
+++ test_platform/tests/test_module_app_views.py
@@ -40,73 +40,76 @@ class ModuleViewsTest(TestCase):
         self.assertIn("退出", res_html)
         self.assertIn("单测模块名称", res_html)
 
-    def test_project_add(self):
-        """测试项目管理新增"""
+    def test_module_add(self):
+        """测试模块管理新增"""
 
-        # self.client.get("/manage/add_project/")
-        response = self.client.post("/manage/add_project/", {"pname":"添加项目","description": "添加项目描述"})
+        p_id = Project.objects.get(pname="项目名称1").id
+        # response = self.client.get("/manage/module_add/")
+        # print("content=", response.content.decode("utf-8"))
+        response=self.client.post("/manage/module_add/", {"project": p_id,"mname":"添加模块1","description": "添加模块描述1"})
         # print("CONTENT",response.content.decode("utf-8"))
         self.assertEqual(response.status_code, 302)
-        response = self.client.get("/manage/project_manage/")
+        response = self.client.get("/manage/module_manage/")
         # 获取返回的页面内容
         res_html = response.content.decode("utf-8")
         self.assertIn("test0001",res_html)
         self.assertIn("退出", res_html)
         #断言新增的数据是否在页面列表上
-        self.assertIn("添加项目", res_html)
+        self.assertIn("添加模块1", res_html)
         #查询数据数据是否已添加,查询返回记录数
-        p_res=Project.objects.filter(pname="添加项目")
+        p_res=Module.objects.filter(mname="添加模块1")
         self.assertEqual(p_res.count(),1)
         #验证数据库数据
-        self.assertEqual(p_res[0].pname, "添加项目")
-        self.assertEqual(p_res[0].description, "添加项目描述")
-        self.assertEqual(p_res[0].status,0)
+        self.assertEqual(p_res[0].mname, "添加模块1")
+        self.assertEqual(p_res[0].description, "添加模块描述1")
 
 
-    def test_project_edit(self):
+
+    def test_module_edit(self):
         """测试项目管理修改"""
-
+        # 获取新增项目的id
+        p_id = Project.objects.get(pname="项目名称1").id
         # 获取新增数据的id，用于修改数据传参
-        pid = Project.objects.get(pname="项目名称1").id
-        # print("pid",pid)
-        response = self.client.post("/manage/manage_edit/"+str(pid)+"/", {"pname":"添加项目修改","description": "添加项目描述修改","status": 1})
-        # print("CONTENT",response.content.decode("utf-8"))
+        mid = Module.objects.get(mname="单测模块名称").id
+        # 注意点：module_edit/mid 后面没有"/" ！！！！！！
+        response = self.client.post("/manage/module_edit/"+str(mid),
+                                    {"project": p_id, "mname": "单测模块名称修改", "description": "单测模块名称描述修改"})
+        print("CONTENT",response.content.decode("utf-8"))
         self.assertEqual(response.status_code, 302)
-        response = self.client.get("/manage/project_manage/")
+        response = self.client.get("/manage/module_manage/")
         # 获取返回的页面内容
         res_html = response.content.decode("utf-8")
         self.assertIn("test0001",res_html)
         self.assertIn("退出", res_html)
         #断言修改的数据是否在页面列表上
-        self.assertIn("添加项目修改", res_html)
+        self.assertIn("单测模块名称修改", res_html)
         #查询数据数据是否已修改,查询返回记录数
-        p_res=Project.objects.filter(id=pid)
-        self.assertEqual(p_res.count(),1)
+        m_res=Module.objects.filter(id=mid)
+        self.assertEqual(m_res.count(), 1)
         #验证数据库数据是否已修改
-        self.assertEqual(p_res[0].pname, "添加项目修改")
-        self.assertEqual(p_res[0].description, "添加项目描述修改")
-        self.assertEqual(p_res[0].status, 1)
+        self.assertEqual(m_res[0].mname, "单测模块名称修改")
+        self.assertEqual(m_res[0].description, "单测模块名称描述修改")
 
 
-    def test_project_delete(self):
+
+    def test_module_delete(self):
         """测试项目管理删除"""
 
         # 获取新增数据的id，用于修改数据传参
-        pid = Project.objects.get(pname="项目名称1").id
-        # print("pid",pid)
-        response = self.client.get("/manage/manage_delete/"+str(pid)+"/")
+        mid = Module.objects.get(mname="单测模块名称").id
+        response = self.client.get("/manage/module_delete/"+str(mid))
         # print("CONTENT",response.content.decode("utf-8"))
         self.assertEqual(response.status_code, 302)
-        response = self.client.get("/manage/project_manage/")
+        response = self.client.get("/manage/module_manage/")
         # 获取返回的页面内容
         res_html = response.content.decode("utf-8")
         self.assertIn("test0001",res_html)
         self.assertIn("退出", res_html)
         #断言删除的数据是否已不在页面列表上
-        self.assertNotIn("项目名称1", res_html)
-        self.assertNotIn("添加项目描述1", res_html)
+        self.assertNotIn("单测模块名称", res_html)
+        self.assertNotIn("单测模块名称描述", res_html)
         #查询数据数据是否已删除,查询返回记录数应为0
-        p_res=Project.objects.filter(id=pid)
-        self.assertEqual(p_res.count(),0)
+        m_res=Module.objects.filter(id=mid)
+        self.assertEqual(m_res.count(),0)
 
 
